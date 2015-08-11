6d23412ffc865ac9d3736cbb34ec5e8c875b6da1
Anderson Brito da Silva <a.brito-da-silva1@newcastle.ac.uk>
2015-08-11 16:06:43 +0100

    Update __init__.py
    adapting, excluding modules io and ui

--- pyhfo/__init__.py
+++ pyhfo/__init__.py
@@ -5,8 +5,8 @@ Created on Thu Apr 16 17:48:52 2015
 @author: anderson
 """
 
-from io import open_dataset,save_dataset
-from ui import plot_eeg
+#from io import open_dataset,save_dataset
+#from ui import plot_eeg
 from core import *
 from version import version
 __version__ = version
